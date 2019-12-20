5625cb2a08f181c1b73f57d8f0bc8024bdacee72
Jesús Fernández <jesusfernxndez10@gmail.com>
2019-12-19 23:04:40 -0500

    probando blog en hosting

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -342,7 +342,7 @@ export default {
       this.newNoticia.correo = userdata.correo;
       this.newNoticia.rango = "Administrador";
     } else {
-      return false
+      return false;
     }
   }
 };
